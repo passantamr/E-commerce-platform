ba70a30d68762d3449e99f816449f44a67f0ad02
passant amr <110687519+passantamr@users.noreply.github.com>
2024-03-22 11:10:57 +0200

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-//const Joi = require('joi');
-const app = express();
-app.use(express.json());
-
-const {productRouter} = require('./routes/product_router');
-//localhost:3000/products
-app.use('/products/',productRouter);
-module.exports = { app, };
