81420ed5d000aa84e7ecc9f1b1391b31a957749e
Mariam-Medhat <65515748+Mariam-Medhat@users.noreply.github.com>
2024-03-25 01:09:16 +0200

    GetProductByCode Function
    Some changes were made in the product_controller, product_router, and app files. The GetProductByCode function fetches a specific product using its ID.

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const express = require("express");
-//const Joi = require('joi');
 const app = express();
 app.use(express.json());
 
 const {productRouter} = require('./routes/product_router');
-//localhost:3000/products
-app.use('/products/',productRouter);
+app.use('/api/categories/',productRouter);
 module.exports = { app, };
+//localhost:3000/products
--- routes/product_router.js
+++ routes/product_router.js
@@ -1,13 +1,13 @@
 const { Router } = require('express');
 const productRouter = Router();
-const productController = require('../controller/product_controller');
+const product_controller= require('../controller/product_controller');
 
 //for simplisty assume link is /
-//productRouter.get('link',productController.APIName);
+productRouter.get('/:id', product_controller.GetProductByCode);
 //productRouter.post('Link',productController.APIName);
 //productRouter.put('Link',productController.APIName);
 //productRouter.delete('Link',productController.APIName);
 
 module.exports = {
-    productRouter
+    productRouter,
 };
