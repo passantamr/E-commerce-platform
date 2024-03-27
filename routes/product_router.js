c338aeba03d7dd73593cc539e14827826e91ccde
Mariam-Medhat <65515748+Mariam-Medhat@users.noreply.github.com>
2024-03-27 03:05:43 +0200

    calling the EditProductByCode function in the routes folder.

--- routes/product_router.js
+++ routes/product_router.js
@@ -5,8 +5,7 @@ const product_controller= require('../controller/product_controller');
 //for simplisty assume link is /
 productRouter.get('/', product_controller.GetAllProducts);
 productRouter.get('/:id', product_controller.GetProductByCode);
-//productRouter.post('Link',productController.APIName);
-//productRouter.put('Link',productController.APIName);
+productRouter.put('/:id',product_controller. EditProductByCode);
 productRouter.delete('/:id',product_controller.DeleteProductByCode);
 productRouter.post('/',product_controller.AddProduct);
 module.exports = {
