412ef382180aca121c31906c56fcc14e3c8e4a0e
afafabdelwahab <113607838+afafabdelwahab@users.noreply.github.com>
2024-04-27 01:23:37 +0300

    changing EditProductByCode end point

--- controller/categories_controller.js
+++ controller/categories_controller.js
@@ -34,23 +34,25 @@ const AddProduct =  (req,res)=>{
     
     
 //const EditProductByCode = async (req, res) => {};
-const EditProductByCode = (req,res)=>{
-    const category=categories.find(c=>c.id===parseInt(req.params.id));
-    if(!category)
-        res.status(404).send('The Category with the given ID not found !');
-    
-
-    const result = validateCategory(req.body);
-    if(result.error){
-        res.status(400).send(result.error.details[0].message);
-        console.log(result);
-        return;
+const EditProductByCode = async (req,res)=>{
+    try{
+        const category = await categories.findOne({code: req.params.code});
+        if(!category){
+            return res.status(404).send('category not found');
+        }
+        if(req.body.name){
+            category.name = req.body.name;
+        }
+        if(req.body.code){
+            category.code = req.body.code;
+        }
+        await category.save();
+        res.status(200).send(category);
+    }catch(error){
+        res.status(400).send(error);
     }
-    category.name=req.body.name;
-    res.send(category);
-    }; 
-
-
+    
+}; 
 //const DeleteProductByCode = async (req, res) => {};
 const DeleteProductByCode = (req,res)=>{
     const category=categories.find(c=>c.id===parseInt(req.params.id));
