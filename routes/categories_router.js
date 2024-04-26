dbf6aa585fc9cb40455ed8df946bf0a98e7ba953
yomnaalwattar <youmna196173@feng.bu.edu.eg>
2024-04-27 01:51:14 +0300

    deleteProductByCode
    edit deleteProductbycode mongoose

--- routes/categories_router.js
+++ routes/categories_router.js
@@ -6,7 +6,7 @@ const category_controller= require('../controller/categories_controller');
 categoryRouter.get('/', category_controller.GetAllProducts);
 categoryRouter.get('/:code', category_controller.GetProductByCode);
 categoryRouter.put('/:id',category_controller. EditProductByCode);
-categoryRouter.delete('/:id',category_controller.DeleteProductByCode);
+categoryRouter.delete('/:code',category_controller.DeleteProductByCode);
 categoryRouter.post('/',category_controller.AddProduct);
 module.exports = {
 
