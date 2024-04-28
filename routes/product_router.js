1810889e8620a1cf6f78cfd8790eda20aae5ec20
passant amr <110687519+passantamr@users.noreply.github.com>
2024-04-28 19:18:00 +0300

    add getproductbycode

--- routes/product_router.js
+++ routes/product_router.js
@@ -4,7 +4,7 @@ const product_controller= require('../controller/product_controller');
 
 //for simplisty assume link is /;
 productRouter.get('/', product_controller.getAllProducts);
-//productRouter.get('/:code', product_controller.getproductByCode);
+productRouter.get('/:code', product_controller.getproductByCode);
 //productRouter.get('/:id', product_controller.getAllProductsByCategoryId);
 //productRouter.delete('/:code/:categoryId',product_controller.deleteproduct);
 //productRouter.put('/:code/:categoryId',product_controller.editproduct);
