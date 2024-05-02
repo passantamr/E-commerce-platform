fc8603413e3820aa59dcf635be279f03e7196982
alaa5radwan <66218719+alaa5radwan@users.noreply.github.com>
2024-05-02 15:53:30 +0300

    simple chande on link api

--- routes/product_router.js
+++ routes/product_router.js
@@ -4,8 +4,8 @@ const product_controller= require('../controller/product_controller');
 
 //for simplisty assume link is /;
 productRouter.get('/', product_controller.getAllProducts);
-productRouter.get('/:code', product_controller.getproductByCode);
-productRouter.get('/:id', product_controller.getAllProductsByCategoryId);
+productRouter.get('/code/:code', product_controller.getproductByCode);
+productRouter.get('/id/:id', product_controller.getAllProductsByCategoryId);
 productRouter.delete('/:code/:categoryId',product_controller.deleteproduct);
 productRouter.put('/:code/:categoryId',product_controller.editproduct);
 productRouter.post('/',product_controller.addproduct);
